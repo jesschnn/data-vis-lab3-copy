97d7c88312ffa31e7d752665037aedbebb922d83
jesschn <jechn34@gmail.com>
2026-03-05 20:03:19 -0500

    up to step 2 lab 5

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,36 @@
   import Project from "$lib/Project.svelte";
   import reading from "$lib/reading.json";
   import ReadingItem from "$lib/ReadingItem.svelte";
+
+  // lab 4
+  import { onMount } from "svelte";
+
+  let githubData = null; // This will eventually hold our Github stats
+  let loading = true; // This will be true *until* the fetch's promise resolves to a value
+  let error = null; // If the API call resulted in an error, it will go into this variable
+
+  
+  onMount(async () => {
+    try {
+        console.log("Page has been mounted!")
+        let response = await {
+            ok: true,
+            json: async () => ({
+                followers: 100,
+                following: 100,
+                public_repos: 100
+            }),
+        };
+        console.log(response);
+        githubData = await response.json();
+        console.log(githubData);
+    } catch (err) {
+        error = err;
+    }
+    loading = false;
+  });
+
+  let data = fetch("https://api.github.com/users/jesschnn");
 </script>
 
 
@@ -27,6 +57,26 @@
 </div>
 <hr>
 
+<div class="github-stats">
+    {#if loading}
+        <p>Loading...</p>
+    {:else if error}
+        <p>Something went wrong: {error.message}</p>
+    {:else}
+        <section>
+            <h2>My GitHub Stats</h2>
+            <dl>
+                <dt>FOLLOWERS</dt>
+                <dd>{githubData.followers}</dd>
+                <dt>FOLLOWING</dt>
+                <dd>{githubData.following}</dd>
+                <dt>PUBLIC REPOSITORIES</dt>
+                <dd>{githubData.public_repos}</dd>
+            </dl>
+        </section>
+    {/if}
+</div>
+
 <div class="project-highlights">
     <br>
     <h2>Latest Projects</h2>
@@ -38,10 +88,6 @@
         {/each}
     </div>
 </div>
-
-
-
-
 
 
 <style>
@@ -58,6 +104,24 @@
         flex: 0.75;
         background-color: color-mix(in oklch, var(--color-accent), canvas 85%);
         padding: 1em;
+    }
+
+    dl {
+        display: grid;
+        grid-template-columns: repeat(3,1fr);
+        gap: 10px;
+    }
+
+    dt {
+        grid-row: 1;
+        color: grey;
+        text-align: left;
+    }
+
+    dd {
+        grid-row: 2;
+        font-size: 150%;
+        margin-left: 0;
     }
 </style>
 
