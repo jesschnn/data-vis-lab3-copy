fee76cf920a8f2d9fbcbb278df3ba40812aaf74b
jesschn <jechn34@gmail.com>
2026-03-06 00:57:02 -0500

    lab 5 images and updates projects

---
+++ src/lib/ProjectNarrative.svelte
@@ -0,0 +1,57 @@
+<script>
+    import Scrolly from "svelte-scrolly";
+    let scrollyProgress = 0;
+    import projects from "./projects.json";
+    let sorted_projects = projects.sort((a, b) => a.year - b.year);
+    let progressPerProject = 100 / sorted_projects.length;
+    $: activeProjectIdx = Math.min(sorted_projects.length-1, Math.floor(scrollyProgress / progressPerProject));
+</script>
+
+<div class="scrolly-wrapper">
+    <Scrolly bind:progress={ scrollyProgress }>
+            <!-- Story here -->
+            {#each sorted_projects as p}
+                <section class="step">
+                    <div class="step-content">
+                        <h3> {p.title} </h3>
+                        <p> {p.story} </p>
+                    </div>
+                </section>
+            {/each}
+            
+            <svelte:fragment slot="viz">
+                <!-- Visualizations here (these will stay sticky) -->
+                <div class="project-detail">
+                    <h3> {sorted_projects[activeProjectIdx].year} </h3>
+                    <img src={sorted_projects[activeProjectIdx].image} 
+                        alt="Image of {sorted_projects[activeProjectIdx].title}">
+
+                </div>
+                
+            </svelte:fragment>
+    </Scrolly>
+</div>
+
+<style>
+    .scrolly-wrapper {
+        width: min(1000ch, 70vw);
+        position: relative;
+        left: 60%;
+        transform: translateX(-50%);
+    }
+
+    .step {
+        min-height: 50vh;
+        padding: 2rem;
+    }
+
+    .step-content {
+        border-left: 0.5em solid color-mix(in oklch, var(--color-accent), canvas 85%);
+        padding: 1.5rem 2rem;
+    }
+
+    .project-detail {
+        padding: 2rem;
+        width: 100%;
+    }
+</style>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -5,9 +5,18 @@
 <script>
   import projects from "$lib/projects.json";
   import Project from "$lib/Project.svelte";
+  import ProjectNarrative from "$lib/ProjectNarrative.svelte";
+  let years = projects.map(proj => proj.year);
+  let range = Math.max(...years) - Math.min(...years);
 </script>
 
-<h1>{projects.length} Projects</h1>
+<h1>{projects.length} Projects over {range} Years</h1>
+<p>Scroll down to see my a timeline of my projects and how they've contributed to my professional and personal life! 
+   (Content generated with Gemini.)</p>
+
+<ProjectNarrative />
+
+<p class="outro">Thanks for scrolling through my project story! Feel free to explore all of the projects at your leisure below.</p>
 <hr>
 
 <div class="projects">
